ee80d417fa1dec17a96448447921fb91f4abd503
sasha <1217435098@qq.com>
2017-03-10 14:46:25 +0800

    1.写了车载进化器的页面，还没做js动效；2.消息中心的下拉刷新还未做处理

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,6 @@
 $(function () {
     //信息下拉刷新
-    $(document).on("pageInit", "#pull-center", function(e, id, page) {
+    $(document).on("pageInit", ".pull-to-refresh-content", function(e, id, page) {
         var $content = $(page).find(".content").on('refresh', function(e) {
             // 模拟2s的加载过程
             setTimeout(function() {
@@ -12,7 +12,7 @@ $(function () {
                 $content.find('.content').prepend(cardHTML);
                 // $(window).scrollTop(0);
                 // 加载完毕需要重置
-                $.pullToRefreshDone('#pull-center');
+                $.pullToRefreshDone('$content');
             }, 2000);
         });
     });
--- src/js/master.js
+++ src/js/master.js
@@ -1,5 +1,16 @@
-$(document).on('click','.button-danger', function () {
-      $.confirm('解绑设备', function () {
-          $.alert('解绑后无法控制设备');
-      });
-  });
+//解绑设备页面的解绑按钮
+$(document).on('click', '.button-danger', function() {
+    $.confirm('解绑设备', function() {
+        $.alert('解绑后无法控制设备');
+    });
+});
+//车载进化器页面的弹出框
+$(document).on('click', '#headpage', function() {
+    $.modal({
+        title: '感谢关注车载净化器！',
+        text: '车载进化器，关注您的每一次呼吸！点击下方"<a>我的设备</a>",检测一下您车内的环境，为它做一次深呼吸吧！尚未绑定设备的，请先点击下方"<a>设备绑定</a>"选择设备绑定，扫描设备二维码进行绑定。',
+        buttons: [{
+            text: '确认'
+        }]
+    })
+});
